1c3a351f8170c8d65cac4402003211cd95a4d47a
Phạm Hoài Kiệt <123@gmail.com>
2025-07-26 16:56:21 +0700

    first commit

---
+++ src/components/Banner.jsx
@@ -0,0 +1,8 @@
+const Banner = () => {
+  return (
+    <div className="w-full h-[600px] bg-banner bg-cover bg-center bg-no-repeat relative">
+      banner
+    </div>
+  );
+};
+export default Banner;
